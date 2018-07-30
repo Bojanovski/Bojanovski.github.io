28e8c1f6977b083958d20e61f0a71661ba51ac91
unknown <bojanlovrovic@gmail.com>
2018-07-30 15:02:42 +0200

    Adding data JSON.

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
 window.onload = function() {
 	
+	var client = new XMLHttpRequest();
+	client.open('GET', 'http://localhost/data/content.json');
+	
+	client.onreadystatechange = function() {
+		alert(client.responseText);
+}
+
+	client.send();
 };
