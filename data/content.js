975ceb2dc40705f1f46c649273a5a717edd8f3c0
unknown <bojanlovrovic@gmail.com>
2018-07-30 18:16:44 +0200

    Adding CSS.

---
+++ data/content.js
@@ -0,0 +1,26 @@
+var CONTENT = 
+{
+	"projects": [
+	{
+		"name": "project_one",
+		"date": 2017
+	},
+	{
+		"name": "project_two",
+		"date": 2018
+	}
+	],
+	
+	"publications": [
+	{
+		"name": "p_one",
+		"url": "https://www.theverge.com/",
+		"date": 2015
+	},
+	{
+		"name": "p_two",
+		"date": 2017
+	}
+	]
+	
+};
--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 
-window.onload = function() {
-	
-	var client = new XMLHttpRequest();
-	client.open('GET', 'http://localhost/data/content.json');
-	
-	client.onreadystatechange = function() {
-		alert(client.responseText);
+function process_single_project(name, date, description, url) {
+	var para = document.createElement("P");
+	var name_txt = document.createTextNode(name);
+	para.appendChild(name_txt);
+	var date_txt = document.createTextNode(" (" + date + ")");
+	para.appendChild(date_txt);
+	return para;
 }
 
-	client.send();
+function process_all_projects(object_list) {
+	for (var i = 0; i < object_list.length; i++) {
+		//console.log(object_list[i].name);
+		var new_project = process_single_project(object_list[i].name, object_list[i].date, object_list[i].description, object_list[i].url);
+		document.getElementById("projectsDIV").appendChild(new_project);     // Append to <div>
+	}
+}
+
+window.onload = function() {
+	for (var key in CONTENT) {
+    if (CONTENT.hasOwnProperty(key)) {
+		//console.log(key);
+		if (key == "projects") {
+			process_all_projects(CONTENT[key]);
+		} else if (key == "publications") {
+			console.log(key + " -> " + CONTENT[key]);
+		} else {
+		}
+    }
+}
 };
